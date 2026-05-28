7a5bc4f707fb5d5830964cc5bb37f414d997312a
Chintan <bat.boy18009@gmail.com>
2026-05-28 16:56:49 +0530

    NewStuff
    NewStuff

--- PhotoEditor/startServer.bat
+++ PhotoEditor/startServer.bat
@@ -3,10 +3,10 @@
 cd /d "%~dp0"
 
 :: Start the Python HTTP server in a new command prompt window
-start "Python HTTP Server" cmd /k python -m http.server 8001
+start "Python HTTP Server" cmd /k python -m http.server 8000
 
 :: Wait a couple of seconds for the server to initialize
 timeout /t 2 /nobreak >nul
 
 :: Open the local server address in the default browser
-start http://localhost:8001/
+start http://localhost:8000/
