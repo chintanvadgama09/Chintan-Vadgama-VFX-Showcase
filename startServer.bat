31583a7b84e586936d57c9ac36540f946e376055
Chintan <bat.boy18009@gmail.com>
2026-06-04 16:42:45 +0530

    New Stuff
    New Stuff

--- startServer.bat
+++ startServer.bat
@@ -2,11 +2,16 @@
 :: Change to the folder where this batch file is located
 cd /d "%~dp0"
 
-:: Start the Python HTTP server in a new command prompt window
-start "Python HTTP Server" cmd /k python -m http.server 8001
+:: Generate a random port number between 8000 and 8999
+set /a PORT=(%RANDOM% %% 1000) + 8000
+
+echo Starting Python HTTP server on random port: %PORT%...
+
+:: Start the Python HTTP server in a new command prompt window using the random port
+start "Python HTTP Server" cmd /k python -m http.server %PORT%
 
 :: Wait a couple of seconds for the server to initialize
 timeout /t 2 /nobreak >nul
 
-:: Open the local server address in the default browser
-start http://localhost:8001/
+:: Open the local server address in the default browser using the random port
+start http://localhost:%PORT%/
